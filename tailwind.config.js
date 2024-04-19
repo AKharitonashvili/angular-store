cf185b8904b886895e29e34e3eef93a47c70dc1b
AKharitonashvili <sandroxari@gmail.com>
2024-04-19 18:34:47 +0400

    feature featured products mobile mock

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ module.exports = {
         primary: "#211C24",
         secondary: "#656565",
         optional: "#909090",
+        "optional-2": "#EDEDED",
+        "priamary-2": "#353535",
         "light-1": "#F5F5F5",
+      },
+      backgroundSize: {
+        "auto-290": "auto 290px",
+        "auto-200": "auto 200px",
+      },
+      backgroundPosition: {
+        "center-top": "center 20%",
       },
     },
   },
