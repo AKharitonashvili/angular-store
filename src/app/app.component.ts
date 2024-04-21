ba01355b4f88adb61775d52fbbb4bf4422ae6bae
AKharitonashvili <sandroxari@gmail.com>
2024-04-21 22:26:07 +0400

    feature featured products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FeaturedProductsComponent } from './featured-products/featured-products.component';
+import { BrowseByCategoryComponent } from './browse-by-category/browse-by-category.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FeaturedProductsComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FeaturedProductsComponent,
+    BrowseByCategoryComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       colors: {
-        primary: "#211C24",
-        secondary: "#656565",
-        optional: "#909090",
-        "optional-2": "#EDEDED",
-        "priamary-2": "#353535",
-        "light-1": "#F5F5F5",
+        primary: '#211C24',
+        secondary: '#656565',
+        optional: '#909090',
+        'optional-2': '#EDEDED',
+        'priamary-2': '#353535',
+        'light-1': '#F5F5F5',
       },
       backgroundSize: {
-        "auto-200": "auto 200px",
-        "auto-290": "auto 290px",
-        "240-340": "240px 340px",
-        "auto-600": "auto 600px",
+        'auto-200': 'auto 200px',
+        'auto-290': 'auto 290px',
+        '240-340': '240px 340px',
+        'auto-500': 'auto 500px',
+        'auto-600': 'auto 600px',
       },
       backgroundPosition: {
-        "center-top": "center 20%",
-        "bottom-right": "bottom right 10%",
-        "bottom-left": "bottom left 10%",
-        "center-left": "center left",
+        'center-top': 'center 20%',
+        'bottom-right': 'bottom right 10%',
+        'bottom-left': 'bottom left 10%',
+        'center-left': 'center left',
+        'top-left-half': 'center left -70%',
+        'top-left-1000': 'center left 1000%',
+        'center-left-150': 'center left -150%',
       },
     },
   },
   plugins: [],
-};
+}
