52e4775558c54141a9a9c244d7b2905cef7754bd
AKharitonashvili <85678274+AKharitonashvili@users.noreply.github.com>
2024-05-20 11:22:45 +0000

    Deploying to prod from @ AKharitonashvili/angular-store@84a861a717c4680315568d5de803f82cb1a21474 🚀

---
+++ chunk-OB3G6XPW.js
@@ -0,0 +1,1 @@
+import{n as t,o as r,t as s}from"./chunk-IATZDMYZ.js";var o=r(s.FAVORITES),n=t(o,e=>e.favorites),F=t(o,e=>e.favorites?.length??0),v=e=>t(o,c=>!!c.favorites?.find(a=>a.id===e)),p=t(o,e=>e.loading),f=t(o,e=>e.error);export{n as a,F as b,v as c};
